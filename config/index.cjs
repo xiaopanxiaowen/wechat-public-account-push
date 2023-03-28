5c7362c95ad9523dccd15d60ae7b82d319a7d8a4
xiaopanxiaowen <129157961+xiaopanxiaowen@users.noreply.github.com>
2023-03-28 16:18:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wx83bfe89f72396714',
+      id: 'oT1h86O3W52XAvawFeuV17GJFhEU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'K4HYwjFUD4nsVGJc7RnaBZ58dc9Drj_jwteExietzEk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
